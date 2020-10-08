2e705927baca5dcb180e50d9f6aa2c7572ff8d06
Zandzakje <DamianTomasila@hotmail.com>
2020-10-08 16:00:10 +0200

    Testing with react start

---
+++ Data/Test_websocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Data
+{
+    class Test_websocket
+    {
+        //test
+        static void Main(string[] args)
+        {
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 8080);
+            TcpClient client = default(TcpClient);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("server started :D");
+                Console.Read();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.Read();
+            }
+        }
+    }
+}
